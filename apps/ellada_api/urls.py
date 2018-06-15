616d854be1aa97c96b683cc6fa8a02ec1b39bf17
developer <mastergowen@gmail.com>
2018-06-15 16:01:06 +0100

    ellada api#2

--- apps/ellada_api/urls.py
+++ apps/ellada_api/urls.py
@@ -3,4 +3,5 @@ from .views import api
 
 urlpatterns = [
     url(r'^(?P<table>.*)/(?P<id>.*)/$', api, name="api"),
+    url(r'^(?P<table>.*)/$', api, name="api"),
     ]
--- apps/ellada_api/views.py
+++ apps/ellada_api/views.py
@@ -12,7 +12,7 @@ def dict_factory(cursor, row):
         d[col[0]] = row[idx]
     return d
 
-def api(request, table, id):
+def api(request, table, id=None):
     conn = sqlite3.connect(DB_path)
     conn.row_factory = dict_factory
     c = conn.cursor()
@@ -20,13 +20,41 @@ def api(request, table, id):
         if(table == 'gallery'):
             if id:
                 c.execute(f"SELECT * FROM gallery WHERE _id_glossary = '{id}'")
-                return JsonResponse(c.fetchone())
+                return JsonResponse(c.fetchall(), safe=False)
             else:
-                c.execute(f"SELECT * FROM glossary ORDER BY glossary.name")
-                return JsonResponse(c.fetchone())
+                c.execute(f"SELECT DISTINCT _id_glossary, name FROM gallery ORDER BY gallery.name")
+                return JsonResponse(c.fetchall(), safe=False)
+        if(table == 'glossary'):
+            if id:
+                c.execute(f"SELECT name, text, _id FROM glossary WHERE _id = '{id}'")
+                return JsonResponse(c.fetchone(), safe=False)
+            else:
+                c.execute(f"SELECT name, _id FROM glossary ORDER BY glossary.name")
+                return JsonResponse(c.fetchall(), safe=False)
+        if(table == 'map'):
+            if id:
+                c.execute(f"SELECT * FROM map WHERE _id = '{id}'")
+                return JsonResponse(c.fetchone(), safe=False)
+            else:
+                c.execute(f"SELECT name, _id FROM map ORDER BY map.name")
+                return JsonResponse(c.fetchall(), safe=False)
+        if(table == 'source'):
+            if id:
+                c.execute(f"SELECT name, text, _id FROM source WHERE _id = '{id}'")
+                return JsonResponse(c.fetchone(), safe=False)
+            else:
+                c.execute(f"SELECT author._id, author.name, author.image, source.name AS sourse_name, source._id AS sourse_id FROM source INNER JOIN author ON author.id = source.id_author ORDER BY author.name")
+                return JsonResponse(c.fetchall(), safe=False)
+        if(table == 'author'):
+            if id:
+                c.execute(f"SELECT author.name, author.text, author._id, publication.text AS publication_text FROM author INNER JOIN publication ON publication.id_author=author.id WHERE author ._id='{id}'")
+                return JsonResponse(c.fetchall(), safe=False)
+            else:
+                c.execute(f"SELECT * FROM author ORDER BY author.name")
+                return JsonResponse(c.fetchall(), safe=False)        
         else:
-            c.execute(f"SELECT * FROM {table} WHERE _id_glossary = '{id}'")
-            return JsonResponse(c.fetchone())
+            c.execute(f"SELECT * FROM {table} WHERE _id = '{id}'")
+            return JsonResponse(c.fetchall(), safe=False)
     except Exception as ex:
         return JsonResponse({'foo': str(ex)})
 

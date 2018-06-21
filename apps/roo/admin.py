1a5f2ae96a87563c53675271b7bfa70dd5877b2a
MasterGowen <mastergowen@gmail.com>
2018-06-21 19:09:53 +0500

    add roo#2

---
+++ apps/roo/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from .models import Course, Platform, Expert, Expertise, Owner
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Course._meta.get_fields()]
+
+
+@admin.register(Platform)
+class PlatformAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Platform._meta.get_fields()]
+
+
+@admin.register(Expert)
+class ExpertAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Expert._meta.get_fields()]
+
+
+@admin.register(Expertise)
+class ExpertiseAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Expertise._meta.get_fields()]
+
+
+@admin.register(Owner)
+class OwnerAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Owner._meta.get_fields()]

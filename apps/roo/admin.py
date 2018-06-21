d7b27adb203d439a792a296c66442d0a2b667301
MasterGowen <mastergowen@gmail.com>
2018-06-21 19:32:27 +0500

    add roo#6

--- apps/roo/admin.py
+++ apps/roo/admin.py
@@ -4,24 +4,42 @@ from .models import Course, Platform, Expert, Expertise, Owner
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in Course._meta.get_fields()]
+    list_display = ("title", "short_description", "description",
+                    "roo_id", "competences", "results",
+                    "grade_tools", "photo", "authors", "prerequisites",
+                    "content", "directions", "subject", "activity",
+                    "language", "enrollment_end_date", "start_date",
+                    "duration", "labor", "certificate", "proctoring_service",
+                    "version", "opened", "admin_email", "expertized",
+                    "domain", "uploaded_roo", "uploaded_prepod",
+                    "platform", "url", "roo_url", "owner",
+                    "permission", "comment"
+                    )
 
 
 @admin.register(Platform)
 class PlatformAdmin(admin.ModelAdmin):
-    list_display = ("title", "person", "connection_form", "connection_date", "contacts" )
+    list_display = ("title", "person", "connection_form", "connection_date", "contacts")
 
 
 @admin.register(Expert)
 class ExpertAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in Expert._meta.get_fields()]
+    list_display = ("login",)
 
 
 @admin.register(Expertise)
 class ExpertiseAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in Expertise._meta.get_fields()]
+    list_display = ("course",
+                    "state",
+                    "date",
+                    "type",
+                    "executed",
+                    "expert",
+                    "supervisor",
+                    "organizer",
+                    )
 
 
 @admin.register(Owner)
 class OwnerAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in Owner._meta.get_fields()]
+    list_display = ("title",)

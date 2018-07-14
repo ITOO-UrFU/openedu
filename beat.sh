0b18ff8b0dade176c014040d172a7a7565462474
vesloguzov <vesloguzov@gmail.com>
2018-07-14 16:01:44 +0500

    roo#12

--- beat.sh
+++ beat.sh
@@ -1,5 +1,5 @@
-/usr/local/itoo/envs/openedu/bin/celery -A openedu beat  &
-/usr/local/itoo/envs/openedu/bin/celery -A openedu worker --loglevel=INFO --concurrency=10 -n worker1@%h &
-/usr/local/itoo/envs/openedu/bin/celery -A openedu worker --loglevel=INFO --concurrency=10 -n worker2@%h &
+/usr/local/itoo/envs/openedu/bin/celery -A openedu beat --without-mingle &
+/usr/local/itoo/envs/openedu/bin/celery -A openedu worker --loglevel=INFO --without-mingle --concurrency=10 -n worker1@%h &
+/usr/local/itoo/envs/openedu/bin/celery -A openedu worker --loglevel=INFO --without-mingle --concurrency=10 -n worker2@%h &
 #/usr/local/itoo/envs/openedu/bin/celery -A openedu worker --loglevel=INFO --concurrency=10 -n worker3@%h &
 #/usr/local/itoo/envs/openedu/bin/celery -A openedu worker --loglevel=INFO --concurrency=10 -n worker4@%h &

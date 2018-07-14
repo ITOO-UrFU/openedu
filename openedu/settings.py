dc268292a0731f2fdb0df929fab8c9c77c133d4d
vesloguzov <vesloguzov@gmail.com>
2018-07-14 15:27:01 +0500

    roo#9

--- openedu/settings.py
+++ openedu/settings.py
@@ -18,7 +18,7 @@ djcelery.setup_loader()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["212.193.82.110", "openedu.urfu.ru", "*"]
 

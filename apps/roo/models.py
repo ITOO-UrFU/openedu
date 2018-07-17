7e80ad13e087bd5589844ffc211f3bf29f2dacde
vesloguzov <vesloguzov@gmail.com>
2018-07-17 17:09:53 +0500

    Модель курса актуализировал #17

--- apps/roo/models.py
+++ apps/roo/models.py
@@ -100,9 +100,9 @@ class Course(models.Model):
                                auth=('vesloguzov@gmail.com', 'ye;yj,jkmitrjlf'))
         courses = request.json()["rows"]
         for course in courses:
-            print(course['global_id'])
             r = requests.get('https://online.edu.ru/api/courses/v0/course/' + course['global_id'],
                              auth=('vesloguzov@gmail.com', 'ye;yj,jkmitrjlf'))
+            print(type(course), course)
             roo_course = Course.objects.get_or_create(roo_id=course['global_id'], defaults={'created_at': course['created_at'], 'finished_at': course['finished_at'], 'title':course['title'] })
             roo_course.save()
 

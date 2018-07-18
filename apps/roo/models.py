7815f1bed1840b2870aca33012f9f5daef0130fd
vesloguzov <vesloguzov@gmail.com>
2018-07-18 17:03:25 +0500

    verify=False

--- apps/roo/models.py
+++ apps/roo/models.py
@@ -108,12 +108,12 @@ class Course(models.Model):
         password = 'ye;yj,jkmitrjlf'
 
         def get_courses_from_page(page_url):
-            request = requests.get(page_url, auth=(login, password))
+            request = requests.get(page_url, auth=(login, password), verify=False)
             response = request.json()
             courses = response["rows"]
             for c in courses:
                 r = requests.get('https://online.edu.ru/api/courses/v0/course/' + c['global_id'],
-                                 auth=('vesloguzov@gmail.com', 'ye;yj,jkmitrjlf'))
+                                 auth=('vesloguzov@gmail.com', 'ye;yj,jkmitrjlf'), verify=False)
                 course = r.json()
                 roo_course = Course.objects.get_or_create(global_id=course['global_id'],
                                                           defaults={'created_at': course['created_at'],

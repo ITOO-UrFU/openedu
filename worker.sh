e56a7852afb3aaa94181f2916cf6fe85f80e58c7
vesloguzov <vesloguzov@gmail.com>
2018-07-18 15:25:58 +0500

    add newest Course field

--- worker.sh
+++
@@ -1,5 +0,0 @@
-/usr/local/itoo/envs/openedu/bin/celery -A openedu beat &
-/usr/local/itoo/envs/openedu/bin/celery -A openedu worker --loglevel=INFO --concurrency=10 -n worker1@%h &
-#/usr/local/itoo/envs/openedu/bin/celery -A openedu worker --loglevel=INFO --concurrency=10 -n worker2@%h &
-#/usr/local/itoo/envs/openedu/bin/celery -A openedu worker --loglevel=INFO --concurrency=10 -n worker3@%h &
-#/usr/local/itoo/envs/openedu/bin/celery -A openedu worker --loglevel=INFO --concurrency=10 -n worker4@%h &

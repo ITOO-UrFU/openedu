ec136389f7386be06fd4e50c97a58bb242f39c45
MasterGowen <mastergowen@gmail.com>
2018-07-19 10:45:01 +0500

    roo_course update/create#5

--- apps/roo/models.py
+++ apps/roo/models.py
@@ -136,7 +136,7 @@ class Course(models.Model):
                 r = requests.get('https://online.edu.ru/api/courses/v0/course/' + c['global_id'],
                                  auth=('vesloguzov@gmail.com', 'ye;yj,jkmitrjlf'), verify=False)
                 course = r.json()
-                roo_course = cls.objects.filter(global_id=course['global_id'])
+                roo_course = cls.objects.filter(global_id=course['global_id'])[0]
 
                 if roo_course:
                     if not any(r.newest for r in roo_course):

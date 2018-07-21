6512a4b8225b56a7283d0750f57ce90e77126f0b
anonimD08 <asdebug@yandex.ru>
2018-07-21 17:29:24 +0500

    loggerTest#13

--- apps/roo/tasks.py
+++ apps/roo/tasks.py
@@ -3,13 +3,14 @@ import datetime
 import json
 from openedu.celery import app
 from .models import Course
+from time import gmtime, strftime
 
 logger = logging.getLogger('celery_logging')
 
 
 @app.task(bind=True)
 def update_courses_from_roo_task(*args):
-    logger.info("RUN TASKS!!!!!")
+    logger.info("Запуск Tasks!!!!!".format(strftime("%Y-%m-%d %H:%M:%S", gmtime())))
     Course.updade_courses_from_roo()
 
 

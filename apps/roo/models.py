0fdf64ee0ca05136260dd9d147c90a178b499e54
anonimD08 <asdebug@yandex.ru>
2018-07-21 15:54:13 +0500

    loggerTest#1

--- apps/roo/models.py
+++ apps/roo/models.py
@@ -145,9 +145,7 @@ class Course(models.Model):
                 r = requests.get(f"https://online.edu.ru/api/courses/v0/course/{c['global_id']}",
                                  auth=('vesloguzov@gmail.com', 'ye;yj,jkmitrjlf'), verify=False)
                 course = r.json()
-                logger.info("global_id")
-                logger.info(course['global_id'])
-                logger.info("!!!!!!!!!!MOOOOODEEEEEEEEEEEEELSSSSSS!!!!!!!!!!!!!!!!")
+                logger.info("global
                 try:
                     roo_course = cls.objects.filter(global_id=course['global_id']).first()
                 except cls.DoesNotExist:

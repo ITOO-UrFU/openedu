1f559a3b0059344ee5af592ed8cc8a0fdb4b3f17
anonimD08 <asdebug@yandex.ru>
2018-07-23 10:57:42 +0500

    platform#2

--- apps/roo/tasks.py
+++ apps/roo/tasks.py
@@ -2,7 +2,7 @@ import logging
 import datetime
 import json
 from openedu.celery import app
-from .models import Course
+from .models import Course, Platform
 from time import gmtime, strftime
 
 logger = logging.getLogger('celery_logging')
@@ -10,8 +10,13 @@ logger = logging.getLogger('celery_logging')
 
 @app.task(bind=True)
 def update_courses_from_roo_task(*args):
-    logger.info("Начали: ".format(strftime("%Y-%m-%d %H:%M:%S", gmtime())))
+    logger.info("Course Начали: {0}".format(strftime("%Y-%m-%d %H:%M:%S", gmtime())))
     Course.updade_courses_from_roo()
 
+@app.task(bind=True)
+def updade_platform_from_roo_task(*args):
+    logger.info("Platform Начали: {0}".format(strftime("%Y-%m-%d %H:%M:%S", gmtime())))
+    Platform.updade_platform_from_roo()
 
-app.control.rate_limit('roo.tasks.update_courses_from_roo_task', '5/m')
+app.control.rate_limit('roo.tasks,update_courses_from_roo_task', '5/m')
+app.control.rate_limit('roo.tasks,update_platform_from_roo_task', '5/m')

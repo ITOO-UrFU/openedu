78cb5dfeef231380e4790e3797ed3a20579bd608
anonimD08 <asdebug@yandex.ru>
2018-07-23 13:12:59 +0500

    platform#10

--- openedu/celery.py
+++ openedu/celery.py
@@ -43,6 +43,12 @@ app.conf.beat_schedule = {
     #     'schedule': 600.0,
     #     'args': (1, 1)
     # },
+
+    'update_platform_from_roo_task': {
+        'task': 'roo.tasks.update_platform_from_roo_task',
+        'schedule': 600.0,
+        'args': (1, 1)
+    },
     'update_courses_from_roo_task': {
         'task': 'roo.tasks.update_courses_from_roo_task',
         'schedule': 240.0,

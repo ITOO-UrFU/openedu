25668eadd9be631e6a8a0ee0fe84d7ac80ca04ea
MasterGowen <mastergowen@gmail.com>
2018-07-25 12:24:05 +0500

    get active tasks#12

--- static/js/tasks_update.js
+++ static/js/tasks_update.js
@@ -2,25 +2,20 @@
 {
     $(document).ready(function()
     {
-        $.ajaxSetup(
-        {
-            type: "POST",
-            cache: false,
-            beforeSend: function() {
-
-            },
-            complete: function() {
-
-            },
-            success: function() {
-
-            }
-        });
-        var $container = $("#active_tasks");
-        $container.load("get_active_tasks/");
         var refreshId = setInterval(function()
         {
-            $container.load("get_active_tasks/");
+            $.post( "get_active_tasks/", "{}", function( data ) {
+                  var items = [];
+
+                  $.each( data["active_tasks"], function( key, val ) {
+                    items.push( "<li>" + val.name+ "</li>" );
+                  });
+
+                  $("#active_tasks").html($( "<ul/>", {
+                    "class": "task_list",
+                    html: items.join( "" )
+                  }));
+                }, "json");
         }, 1000);
     });
 })(jQuery);

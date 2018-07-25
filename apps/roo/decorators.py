42253c2065053802e76f60e330a2d25afb237887
MasterGowen <mastergowen@gmail.com>
2018-07-25 10:34:59 +0500

    roo decorator#1

--- apps/roo/decorators.py
+++ apps/roo/decorators.py
@@ -9,7 +9,7 @@ def roo_member_required(view_func=None, redirect_field_name=REDIRECT_FIELD_NAME,
     member, redirecting to the login page if necessary.
     """
     actual_decorator = user_passes_test(
-        lambda u: u.is_active and u.is_staff and u.groups.filter(name="roo").exists(),
+        lambda u: u.is_active and u.is_staff or u.groups.filter(name="roo").exists(),
         login_url=login_url,
         redirect_field_name=redirect_field_name
     )

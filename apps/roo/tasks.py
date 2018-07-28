f4fa3761cfce19496f0ec61ea9699e69497215b7
vesloguzov <vesloguzov@gmail.com>
2018-07-28 15:34:51 +0500

    openprofession upload tasks#4

--- apps/roo/tasks.py
+++ apps/roo/tasks.py
@@ -11,34 +11,34 @@ from .models import Course, Platform, Owner, Area, Direction
 # logger = logging.getLogger('celery_logging')
 
 
-@app.task(bind=True)
-def update_courses_from_roo_task(self, *args):
-    # logger.info("Course Начали: {0}".format(strftime("%Y-%m-%d %H:%M:%S", gmtime())))
-    Course.updade_courses_from_roo()
-
-
-@app.task(bind=True)
-def update_platform_from_roo_task(*args):
-    # logger.info("Platform Начали: {0}".format(strftime("%Y-%m-%d %H:%M:%S", gmtime())))
-    Platform.get()
-
-
-@app.task(bind=True)
-def update_owner_from_roo_task(*args):
-    # logger.info("Owner Начали: {0}".format(strftime("%Y-%m-%d %H:%M:%S", gmtime())))
-    Owner.get()
-
-
-@app.task(bind=True)
-def update_areas_from_roo_task(*args):
-    # logger.info("Areas Начали: {0}".format(strftime("%Y-%m-%d %H:%M:%S", gmtime())))
-    Area.get()
-
-
-@app.task(bind=True)
-def update_direction_from_roo_task(*args):
-    # logger.info("Direction Начали: {0}".format(strftime("%Y-%m-%d %H:%M:%S", gmtime())))
-    Direction.get()
+# @app.task(bind=True)
+# def update_courses_from_roo_task(self, *args):
+#     # logger.info("Course Начали: {0}".format(strftime("%Y-%m-%d %H:%M:%S", gmtime())))
+#     Course.updade_courses_from_roo()
+#
+#
+# @app.task(bind=True)
+# def update_platform_from_roo_task(*args):
+#     # logger.info("Platform Начали: {0}".format(strftime("%Y-%m-%d %H:%M:%S", gmtime())))
+#     Platform.get()
+#
+#
+# @app.task(bind=True)
+# def update_owner_from_roo_task(*args):
+#     # logger.info("Owner Начали: {0}".format(strftime("%Y-%m-%d %H:%M:%S", gmtime())))
+#     Owner.get()
+#
+#
+# @app.task(bind=True)
+# def update_areas_from_roo_task(*args):
+#     # logger.info("Areas Начали: {0}".format(strftime("%Y-%m-%d %H:%M:%S", gmtime())))
+#     Area.get()
+#
+#
+# @app.task(bind=True)
+# def update_direction_from_roo_task(*args):
+#     # logger.info("Direction Начали: {0}".format(strftime("%Y-%m-%d %H:%M:%S", gmtime())))
+#     Direction.get()
 
 # app.control.rate_limit('roo.tasks,update_courses_from_roo_task', '5/m')
 # app.control.rate_limit('roo.tasks,update_platform_from_roo_task', '5/m')

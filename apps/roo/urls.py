6f50b905a3182bbc2b497b5a320e648463f78eea
anonimD08 <asdebug@yandex.ru>
2018-08-03 11:44:49 +0500

    url table

--- apps/roo/urls.py
+++ apps/roo/urls.py
@@ -3,6 +3,6 @@ from .views import index, get_active_tasks, course_table
 
 urlpatterns = [
     url(r'^$', index, name='index'),
-    url(r'^$', course_table, name='course_table'),
+    url(r'course_table/', course_table, name='course_table'),
     url(r'^get_active_tasks/$', get_active_tasks, name='get_active_tasks'),
 ]

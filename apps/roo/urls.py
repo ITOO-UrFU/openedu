68bb0e762047af16aceba6f67ab1c1a8b6d62885
MasterGowen <mastergowen@gmail.com>
2018-08-04 14:39:14 +0500

    course_table#28

--- apps/roo/urls.py
+++ apps/roo/urls.py
@@ -4,5 +4,5 @@ from .views import data, get_active_tasks, course_table
 urlpatterns = [
     url(r'data/$', data, name='data'),
     url(r'course_table/', course_table, name='course_table'),
-    url(r'^get_active_tasks/$', get_active_tasks, name='get_active_tasks'),
+    url(r'data/get_active_tasks/$', get_active_tasks, name='get_active_tasks'),
 ]

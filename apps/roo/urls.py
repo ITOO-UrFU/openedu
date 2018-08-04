92ce28ab57dd03f933a63df5c06aea0874711f6a
MasterGowen <mastergowen@gmail.com>
2018-08-04 16:21:54 +0500

    course_table#45 lol

--- apps/roo/urls.py
+++ apps/roo/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from .views import data, get_active_tasks, course_table, CourseUpdate
 
 urlpatterns = [
-    url(r'^(?P<pk>[0-9]{2})', CourseUpdate.as_view(), name="detail"),
+    url(r'^(?P<pk>[0-9])', CourseUpdate.as_view(), name="detail"),
     url(r'data/$', data, name='data'),
     url(r'course_table/', course_table, name='course_table'),
     url(r'data/get_active_tasks/$', get_active_tasks, name='get_active_tasks'),

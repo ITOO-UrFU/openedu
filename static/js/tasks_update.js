e74c4f7bdb0fd2489952412de717f430fb6cc4ce
MasterGowen <mastergowen@gmail.com>
2018-08-04 14:56:41 +0500

    course_table#33

--- static/js/tasks_update.js
+++ static/js/tasks_update.js
@@ -7,14 +7,14 @@
 
 
                     $.each(data["active_tasks"], function (key, val) {
-                        items.push("<li>" + val.name + "</li>");
+                        items.push("<tr><td>" + (parseInt(key)+1) + "</td>"+"<td>" + val.name + "</td>"+"<td>" + $.format.date(new Date(val.time_start*1000), 'dd/MM/yy HH:mm') + "</td></tr>");
                     });
                 }
                 else {
-                    items.push("<p style='margin-left:1em'>Нет активных задач</p>")
+                    items.push("<p style='margin-left:2em'>Нет активных задач</p>")
                 }
 
-                $("#active_tasks").html($("<ul/>", {
+                $("#active_tasks_container tbody").html($("<tr/>", {
                     "class": "task_list",
                     html: items.join("")
                 }));

a92d28b2dd9c98bad74def20a2918e06eca0e194
MasterGowen <mastergowen@gmail.com>
2018-08-04 13:03:22 +0500

    course_table#6

--- apps/roo/models.py
+++ apps/roo/models.py
@@ -526,7 +526,7 @@ class RooTable(tables.Table):
     class Meta:
         model = Course
         exclude = ("credits", "record_end_at", "global_id", "created_at", "visitors_rating", "duration", "finished_at", "language", "content", "started_at", "started_at", "requirements", "competences", "accreditation", "description", "image")
-        fields = ("title", "platform", "institution", "communication_owner", "communication_platform", "expertise_status", "passport_status", "passport_status", "required_ratings_state", "unforced_ratings_state")
+        fields = ("title", "partner", "institution", "communication_owner", "communication_platform", "expertise_status", "passport_status", "passport_status", "required_ratings_state", "unforced_ratings_state")
 
     competences = tables.TemplateColumn('{{ record.description | truncatewords_html:5 |safe}}')
     description = tables.TemplateColumn('{{ record.description | truncatewords_html:5 |safe}}')
@@ -539,4 +539,3 @@ class RooTable(tables.Table):
     passport_status = ChoiceColumn(Course.PASSPORT_STATES)
     required_ratings_state = ChoiceColumn(Course.REQUIRED_RATINGS_STATES)
     unforced_ratings_state = ChoiceColumn(Course.UNFORCED_RATINGS_STATES)
-    platform = tables.RelatedLinkColumn()

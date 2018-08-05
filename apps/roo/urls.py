dd63266fe464bc633da37f05e1c3902f723c89a1
MasterGowen <___Cantd3stroY>
2018-08-05 12:37:43 +0500

    expertises#2

--- apps/roo/urls.py
+++ apps/roo/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
+from django.views.generic.base import RedirectView
 from .views import data, get_active_tasks, courses, CourseUpdate, expertises
 
 urlpatterns = [
+    url(r'^.*$', RedirectView.as_view(url='/roo/courses/', permanent=False), name='index'),
     url(r'^(?P<pk>[0-9])', CourseUpdate.as_view(), name="detail"),
     url(r'data/$', data, name='data'),
     url(r'courses/', courses, name='courses'),

bf3e9183e693f36269aabd510d891ce286e5f403
MasterGowen <mastergowen@gmail.com>
2018-08-09 12:18:38 +0500

    expertise#5

--- apps/roo/models.py
+++ apps/roo/models.py
@@ -558,6 +558,7 @@ class ExpertisesTable(tables.Table):
     class Meta:
         model = Expertise
         template_name = "django_tables2/bootstrap.html"
-        fields = ("id", "course", "state", "date", "type", "executed", "expert", "supervisor", "organizer", "comment")
-        id = tables.TemplateColumn(
-            '<a href="#" onClick="ExpertiseEdit=window.open(\'http://openedu.urfu.ru/roo/expertise/{{ record.id }}\',\'{{ record.course }}\',width=600,height=300); return false;">{{ record.course }}</a')
+        fields = ("course", "state", "date", "type", "executed", "expert", "supervisor", "organizer", "comment")
+
+    course = tables.TemplateColumn(
+        '<a href="#" onClick="ExpertiseEdit=window.open(\'http://openedu.urfu.ru/roo/expertise/{{ record.id }}\',\'{{ record.course }}\',width=600,height=300); return false;">{{ record.course }}</a')

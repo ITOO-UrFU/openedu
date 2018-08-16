8405658a1c01ef611f5d4e4bae5ad0e5b0a5cc13
vesloguzov <vesloguzov@gmail.com>
2018-08-16 16:45:09 +0500

    test#6

--- apps/roo/views.py
+++ apps/roo/views.py
@@ -28,7 +28,7 @@ def upload_from_json(request):
         # logger.info(courses)
         i = 0
         for course in courses:
-            if course["title"] is None or course["platform"] is None or course["owner"] is None:
+            if course["title"] == None or course["platform"] == None or course["owner"] == None:
                 i += 1
                 logger.info(course)
                 logger.info("!!!!!!!!!!!!!!!!!!!!!!: ", i)

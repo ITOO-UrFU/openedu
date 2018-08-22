26ad54cc7fc9cf3a738e9c5017b6f69879e0fe2d
vesloguzov <vesloguzov@gmail.com>
2018-08-22 18:32:21 +0500

    expertise#3

--- apps/roo/views.py
+++ apps/roo/views.py
@@ -71,7 +71,7 @@ def add_expertises(course, our_course):
             # if course["expert"]:
             #     expert =
             expertise = Expertise.objects.create(course=our_course, supervisor=course["supervisor"], type=_type,
-                                     state=course["state"], organizer=course["organizer"], ex_date=course["date"], executed=3 if course["expertise_status"].strip().lower() == "да" else 0)
+                                     state=course["state"], organizer=course["organizer"], ex_date=course["date"], executed=True if course["expertise_status"].strip().lower() == "да" else False)
 
             if course.get("expert", ""):
                 if len(str(course["expert"]).strip()) > 0:

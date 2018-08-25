2d9f16233d815e066cb4bb1900fc32e747f9a779
vesloguzov <vesloguzov@gmail.com>
2018-08-25 16:11:22 +0500

    course edit #78

--- apps/roo/views.py
+++ apps/roo/views.py
@@ -282,7 +282,7 @@ def courses_list(request):
 def courses_edit(request):
     # context = dict()
     # return render(request, "roo/courses_edit.html", context)
-    data = serialize('json', Course.objects.all())
+    data = serialize('xml', Course.objects.all())
     return_data = []
     for course in json.loads(data):
         new_course = course['fields']

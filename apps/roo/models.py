088c3745968fee8be2b0880e603399452b13e3db
vesloguzov <vesloguzov@gmail.com>
2018-09-10 12:40:48 +0500

    openprofession form #8

--- apps/roo/models.py
+++ apps/roo/models.py
@@ -13,6 +13,15 @@ import django_tables2 as tables
 
 # logger = logging.getLogger('celery_logging')
 
+from django import template
+from django.contrib.auth.models import Group
+
+register = template.Library()
+
+@register.filter(name='has_group')
+def has_group(user, group_name):
+    group = Group.objects.get(name=group_name)
+    return group in user.groups.all()
 
 class Base(models.Model):
     class Meta:

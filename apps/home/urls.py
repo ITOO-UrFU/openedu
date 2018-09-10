4855ae1e7ec3d8b47939b29f31102d3095a93d3b
MasterGowen <mastergowen@gmail.com>
2018-09-10 18:43:27 +0500

    login#5

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from .views import page_view, login
 
 urlpatterns = [
+    url(r'^login$', login, name='login'),
     url(r'^$', page_view, name='page_view'),
     url(r'^(?P<link>.*)/$', page_view, name='page_view'),
-    url(r'^login$', login, name='login'),
 ]

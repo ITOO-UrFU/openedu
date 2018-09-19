17d43865d0aeacf5f0fc8de2ee566b4746eed8a3
vesloguzov <vesloguzov@gmail.com>
2018-09-19 15:18:56 +0500

    lol

--- apps/roo/urls.py
+++ apps/roo/urls.py
@@ -2,13 +2,13 @@ from django.conf.urls import url
 from django.views.generic.base import RedirectView, TemplateView
 from .views import data, get_active_tasks, courses, CourseUpdate, expertises, ExpertiseUpdate, upload_from_json, \
     courses_list, courses_edit, ExpertiseCreate, TeacherCreate, upload_expertises, CourseCreate, TableCourseUpdate, \
-    expertises_list, expertises_edit, TableExpertiseUpdate, upload_comments, expertises_list, visible_columns_courses, \
-    visible_columns_expertises
+    expertises_list, expertises_edit, TableExpertiseUpdate, upload_comments, expertises_list, visible_columns_courses, visible_columns_expertises, ExpertiseUpdate1
 
 urlpatterns = [
     url(r'^$', RedirectView.as_view(url='/roo/courses/', permanent=False), name='index'),
     url(r'^(?P<pk>\d+)', CourseUpdate.as_view(), name="detail"),
     url(r'^expertise/(?P<pk>\d+)/$', ExpertiseUpdate.as_view(), name="detail"),
+    url(r'^expertise1/(?P<pk>\d+)/$', ExpertiseUpdate1.as_view(), name="detail1"),
     url(r'data/$', data, name='data'),
     url(r'visible_columns_courses/', visible_columns_courses, name='visible_columns_courses'),
     url(r'visible_columns_expertises/', visible_columns_expertises, name='visible_columns_expertises'),
@@ -30,15 +30,3 @@ urlpatterns = [
     url(r'^close/$', TemplateView.as_view(template_name='roo/close.html')),
 
 ]
-
-chapters = [s for s in structure.values() if s['block_type'] == 'chapter']
-for chapter in chapters:
-    sequentials = chapter['children']
-    for sequential_id in sequentials:
-        sequential = structure[sequential_id]
-        for block_id in sequential['children']:
-            block = structure[block_id]
-            for item_id in block['children']:
-                item = structure[item_id]
-
-    module_order = module_order + 1

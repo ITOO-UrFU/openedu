ed39f139861f5a16cfd93f8b7688fb6ea09171e5
vesloguzov <vesloguzov@gmail.com>
2018-09-24 18:23:50 +0500

    lol #18

--- apps/roo/urls.py
+++ apps/roo/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import url
 from django.views.generic.base import RedirectView, TemplateView
 from .views import data, get_active_tasks, courses, CourseUpdate, expertises, ExpertiseUpdate, upload_from_json, \
     courses_list, courses_edit, ExpertiseCreate, TeacherCreate, upload_expertises, CourseCreate, TableCourseUpdate, \
-    expertises_list, expertises_edit, TableExpertiseUpdate, upload_comments, expertises_list, visible_columns_courses, visible_columns_expertises, ExpertiseUpdate1, new_expertise
+    expertises_list, expertises_edit, TableExpertiseUpdate, upload_comments, expertises_list, visible_columns_courses, \
+    visible_columns_expertises, ExpertiseUpdate1, new_expertise, some_view
 
 urlpatterns = [
     url(r'^$', RedirectView.as_view(url='/roo/courses/', permanent=False), name='index'),
@@ -10,6 +11,7 @@ urlpatterns = [
     url(r'^expertise/(?P<pk>\d+)/$', ExpertiseUpdate.as_view(), name="detail"),
     url(r'^expertise1/(?P<pk>\d+)/$', ExpertiseUpdate1.as_view(), name="detail1"),
     url(r'data/$', data, name='data'),
+    url(r'some_view/', some_view, name='some_view'),
     url(r'visible_columns_courses/', visible_columns_courses, name='visible_columns_courses'),
     url(r'visible_columns_expertises/', visible_columns_expertises, name='visible_columns_expertises'),
     url(r'courses/', courses_list, name='courses_list'),

dad8ab045bbcbd2965bf4464153e08d616bc07f4
vesloguzov <vesloguzov@gmail.com>
2018-10-16 14:25:30 +0500

    export #3006

--- apps/roo/models.py
+++ apps/roo/models.py
@@ -224,8 +224,10 @@ class Course(models.Model):
     credits = models.CharField("Массив перезачётов", blank=True, null=True, max_length=512)
     record_end_at = models.CharField("Дата окончания записи на курс", blank=True, null=True, max_length=512)
     title = models.CharField("Наименование", blank=True, null=True, max_length=512)
-    image = models.URLField("Изображение курса", blank=True, null=True)
     institution = models.ForeignKey("Owner", verbose_name="Правообладатель", blank=True, null=True)
+    partner = models.ForeignKey("Platform", verbose_name="Платформа", null=True)
+    external_url = models.CharField("Ссылка на онлайн-курс на сайте Платформы", blank=True, null=True, max_length=512)
+    image = models.URLField("Изображение курса", blank=True, null=True)
     global_id = models.CharField("ИД курса на РОО", blank=True, null=True, max_length=512)
     created_at = models.CharField("Дата создания онлайн-курса", blank=True, null=True, max_length=512)
     visitors_rating = models.CharField("Оценка посетителей РОО", blank=True, null=True,
@@ -245,11 +247,11 @@ class Course(models.Model):
     verified_cert = models.BooleanField("Возможность получить подтвержденный сертификат", default=False)
     language = models.CharField("Язык контента", blank=True, null=True, max_length=512)
     course_item_url = models.CharField("", blank=True, null=True, max_length=512)  # неизвестная вестчь
-    partner = models.ForeignKey("Platform", verbose_name="Платформа", null=True)
+
     content = models.TextField("Содержание онлайн-курса", blank=True, null=True, max_length=4096)
     started_at = models.CharField("Дата ближайшего запуска", blank=True, null=True, max_length=512)
     rating = models.CharField("Рейтинг пользователей", blank=True, null=True, max_length=512)
-    external_url = models.CharField("Ссылка на онлайн-курс на сайте Платформы", blank=True, null=True, max_length=512)
+
     lectures_number = models.IntegerField("Количество лекций", blank=True, null=True)
     version = models.IntegerField("Версия курса", default=1)
     activities = models.ManyToManyField("Area", verbose_name="Массив идентификаторов областей деятельности", blank=True,

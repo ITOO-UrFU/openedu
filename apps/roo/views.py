706c02f0522233b16b3423f54a496416ea2fa06c
vesloguzov <vesloguzov@gmail.com>
2018-10-23 14:55:48 +0500

    export #3014

--- apps/roo/views.py
+++ apps/roo/views.py
@@ -552,14 +552,15 @@ def TableExpertiseUpdate(request):
         return HttpResponse(status=500)
 
 
-def course_json(request, pk):
+def course_json(request, course_id):
     if request.method == "GET":
-        course = Course.objects.get(pk=pk)
+        course = Course.objects.get(pk=course_id)
         data = serialize('json', [course, ])
         struct = json.loads(data)[0]
         new_course = struct['fields']
         new_course['pk'] = struct['pk']
         return JsonResponse(new_course)
+
 
 def TableCourseUpdate(request):
     if request.method == "POST":

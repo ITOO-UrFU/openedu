85487aa1930aead9834a54065507d2b597c0fecd
vesloguzov <vesloguzov@gmail.com>
2018-10-30 17:26:24 +0500

    export #3025

--- apps/roo/views.py
+++ apps/roo/views.py
@@ -575,7 +575,7 @@ def course_json(request, course_id):
 
         new_course["enrollment_finished_at"] = new_course["record_end_at"]
 
-        new_course['direction'] = [x.code for x in Direction.objects.filter(pk__in=new_course['directions'])]
+        new_course['teachers'] = [{"image": x.image, "display_name": x.title, "description": x.description} for x in Teacher.objects.filter(pk__in=new_course['teachers'])]
 
 
         new_course['direction'] = [x.code for x in Direction.objects.filter(pk__in=new_course['directions'])]

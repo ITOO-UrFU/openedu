e59e13c0480746dd7c64b7d81660e29e611ec9cf
MasterGowen <mastergowen@gmail.com>
2018-12-01 18:39:16 +0500

    roo_course#45

--- apps/roo/models.py
+++ apps/roo/models.py
@@ -627,6 +627,7 @@ class Course(models.Model):
                 elif field_name == "credits":
                     return str(a) == str(b)
                 elif field_name == "accreditation":
+                    print(a, '---', b)
                     if not a or a == "":
                         a = None
                     if not b:

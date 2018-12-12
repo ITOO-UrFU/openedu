327cec35a9eeb12ed74d3eb2d834cf6f38edc54d
vesloguzov <vesloguzov@gmail.com>
2018-12-12 11:36:36 +0500

    diff#23

--- apps/roo/models.py
+++ apps/roo/models.py
@@ -683,7 +683,7 @@ class Course(models.Model):
                             diff[field] = {"our": almost_equal_a, "roo": almost_equal_b}
                             # print(field, almost_equal(getattr(roo_course, field), course[field], field), getattr(roo_course, field), course[field])
                     if len(diff.keys()) > 0:
-                        course_diff, created = CourseDiff.objects.get_or_create(course=roo_course)
+                        course_diff, created = CourseDiff.objects.get_or_create(course=roo_course)[0]
                         course_diff.diff = json.dumps(diff)
                         # .create(course=roo_course, diff=json.dumps(diff))
                         course_diff.save()

f6cd9b6808dbd2b21fde9c133ed95f41b9e38e4e
MasterGowen <mastergowen@gmail.com>
2019-01-15 17:49:25 +0500

    #19

--- apps/roo/admin.py
+++ apps/roo/admin.py
@@ -236,7 +236,7 @@ class CourseAdmin(ImportExportModelAdmin):
     actions_on_top = True
     actions_on_bottom = True
     resource_class = CourseResource
-    list_display = ("title", "get_platform", "institution", "get_description", "global_id")
+    list_display = ("title", "get_platform", "institution", "get_description", "global_id", "language")
     list_filter = ("roo_status", "in_archive", GIDListFilter)
     # filter_horizontal = ("roo_status", "in_archive",)
     search_fields = ("title", "global_id")
